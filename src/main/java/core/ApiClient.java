03b76c3422ef7f1a2a3c02cc385fdd7099a77ba5
Somdle <58317426+Somdle@users.noreply.github.com>
2024-05-28 00:29:40 +0900

    http post 완료

--- src/main/java/core/ApiClient.java
+++ src/main/java/core/ApiClient.java
@@ -50,7 +50,47 @@ public class ApiClient {
         }
     }
 
+    public static JSONObject HttpPost(String params_id, String body) {
+        try {
+            // JWT 토큰 생성
+            String token = JwtUtil.generateJwtToken("pcu_project");
+
+            // 문자열 인코딩 (http 에러 방지)
+            String encoded_params_id = URLEncoder.encode(params_id, "UTF-8");
+
+            // HttpClient 생성
+            HttpClient client = HttpClient.newHttpClient();
+
+            // HttpRequest 생성
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(apiUrl + encoded_params_id))
+                    .header("Authorization", "Bearer " + token)
+                    .POST(HttpRequest.BodyPublishers.ofString(body)) // body 추가
+                    .build();
+
+            // 요청 보내고 응답 받기
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            // 응답 코드 확인
+            if (response.statusCode() == 200) {
+                // 응답 본문을 JSON 객체로 변환
+                JSONObject jsonResponse = new JSONObject(response.body());
+
+                // JSON 데이터 출력
+                // System.out.println(jsonResponse.toString(4)); // JSON을 예쁘게 출력
+                return jsonResponse;
+            } else {
+                System.out.println("HTTP error code: " + response.statusCode());
+                return null;
+            }
+        } catch (Exception e) {
+            System.out.println("Http POST error: " + e.toString());
+            return null;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println(HttpGet("학생 식별자"));
+//        System.out.println(HttpGet("학생 식별자"));
+        System.out.println(HttpPost("학생 식별자", "{\"studentId\":\"학생 ID\",\"major\":\"전공\",\"grade\":\"학년\",\"name\":\"학생 이름\",\"lectures\":[\"수강 강의 ID 1\",\"수강 강의 ID 2\"],\"_id\":\"학생 식별자\"}"));
     }
 }
