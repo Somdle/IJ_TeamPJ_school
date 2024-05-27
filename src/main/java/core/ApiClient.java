04f15e6cdc116ca713e6319ef793c529de0d6841
Somdle <58317426+Somdle@users.noreply.github.com>
2024-05-28 01:00:53 +0900

    REST 개선

--- src/main/java/core/ApiClient.java
+++ src/main/java/core/ApiClient.java
@@ -9,9 +9,9 @@ import java.net.http.HttpResponse;
 import java.net.URLEncoder;
 
 public class ApiClient {
-    static String apiUrl = "http://localhost:3000/api/students?id=";
+    static String apiUrl = "http://localhost:3000/api";
 
-    private static HttpRequest generateRequest(String params_id, String method, String body) throws Exception {
+    private static HttpRequest generateRequest(String domain, String params_id, String method, String body) throws Exception {
         // JWT 토큰 생성
         String token = JwtUtil.generateJwtToken("pcu_project");
 
@@ -20,7 +20,7 @@ public class ApiClient {
 
         // HttpRequest 생성
         HttpRequest.Builder requestBuilder = HttpRequest.newBuilder()
-                .uri(URI.create(apiUrl + encoded_params_id))
+                .uri(URI.create(apiUrl + "/" + domain + "?id=" + encoded_params_id))
                 .header("Authorization", "Bearer " + token);
 
         if ("POST".equals(method)) {
@@ -36,11 +36,11 @@ public class ApiClient {
         }
     }
 
-    public static JSONObject HttpGet(String params_id) {
+    public static JSONObject HttpGet(String domain, String params_id) {
         try {
             // 요청 보내고 응답 받기
             HttpResponse<String> response = HttpClient.newHttpClient()
-                    .send(generateRequest(params_id, "GET", null), HttpResponse.BodyHandlers.ofString());
+                    .send(generateRequest(domain, params_id, "GET", null), HttpResponse.BodyHandlers.ofString());
 
             // 응답 코드 확인
             if (response.statusCode() == 200) {
@@ -56,11 +56,11 @@ public class ApiClient {
         }
     }
 
-    public static JSONObject HttpPost(String params_id, String body) {
+    public static JSONObject HttpPost(String domain, String params_id, String body) {
         try {
             // 요청 보내고 응답 받기
             HttpResponse<String> response = HttpClient.newHttpClient()
-                    .send(generateRequest(params_id, "POST", body), HttpResponse.BodyHandlers.ofString());
+                    .send(generateRequest(domain, params_id, "POST", body), HttpResponse.BodyHandlers.ofString());
 
             // 응답 코드 확인
             if (response.statusCode() == 200) {
@@ -76,11 +76,11 @@ public class ApiClient {
         }
     }
 
-    public static JSONObject HttpPut(String params_id, String body) {
+    public static JSONObject HttpPut(String domain, String params_id, String body) {
         try {
             // 요청 보내고 응답 받기
             HttpResponse<String> response = HttpClient.newHttpClient()
-                    .send(generateRequest(params_id, "PUT", body), HttpResponse.BodyHandlers.ofString());
+                    .send(generateRequest(domain, params_id, "PUT", body), HttpResponse.BodyHandlers.ofString());
 
             // 응답 코드 확인
             if (response.statusCode() == 200) {
@@ -96,11 +96,11 @@ public class ApiClient {
         }
     }
 
-    public static JSONObject HttpDelete(String params_id) {
+    public static JSONObject HttpDelete(String domain, String params_id) {
         try {
             // 요청 보내고 응답 받기
             HttpResponse<String> response = HttpClient.newHttpClient()
-                    .send(generateRequest(params_id, "DELETE", null), HttpResponse.BodyHandlers.ofString());
+                    .send(generateRequest(domain, params_id, "DELETE", null), HttpResponse.BodyHandlers.ofString());
 
             // 응답 코드 확인
             if (response.statusCode() == 200) {
@@ -117,9 +117,9 @@ public class ApiClient {
     }
 
     public static void main(String[] args) {
-         System.out.println(HttpGet("학생 식별자"));
-//         System.out.println(HttpPost("학생 식별자", "{\"studentId\":\"학생 ID\",\"major\":\"전공\",\"grade\":\"학년\",\"name\":\"학생 이름\",\"lectures\":[\"수강 강의 ID 1\",\"수강 강의 ID 2\"],\"_id\":\"학생 식별자\"}"));
-//         System.out.println(HttpPut("학생 식별자", "{\"studentId\":\"학생 ID\",\"major\":\"전공 수정\",\"grade\":\"학년 수정\",\"name\":\"학생 이름 수정\",\"lectures\":[\"수강 강의 ID 1 수정\",\"수강 강의 ID 2 수정\"],\"_id\":\"학생 식별자\"}"));
-//         System.out.println(HttpDelete("학생 식별자"));
+         System.out.println(HttpGet("students", "학생 식별자"));
+//         System.out.println(HttpPost("students", "학생 식별자", "{\"studentId\":\"학생 ID\",\"major\":\"전공\",\"grade\":\"학년\",\"name\":\"학생 이름\",\"lectures\":[\"수강 강의 ID 1\",\"수강 강의 ID 2\"],\"_id\":\"학생 식별자\"}"));
+//         System.out.println(HttpPut("students", "학생 식별자", "{\"studentId\":\"학생 ID\",\"major\":\"전공 수정\",\"grade\":\"학년 수정\",\"name\":\"학생 이름 수정\",\"lectures\":[\"수강 강의 ID 1 수정\",\"수강 강의 ID 2 수정\"],\"_id\":\"학생 식별자\"}"));
+//         System.out.println(HttpDelete("students", "학생 식별자"));
     }
 }
