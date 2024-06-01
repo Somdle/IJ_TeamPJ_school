036d6298a8265da03e1a5d7c3423c21a549b74ed
Somdle <rbtjr00@gmail.com>
2024-06-01 20:59:08 +0900

    학생, 강의 정보를 받아와 객체에 넣기 완료

--- src/main/java/core/App.java
+++ src/main/java/core/App.java
@@ -1,7 +1,19 @@
 package core;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // 학생 정보 받아오기
+        Student student = new Student(ApiClient.HttpGet("students", "학생 식별자"));
+
+        // 강의 정보 받아오기
+        Lecture lecture = new Lecture(ApiClient.HttpGet("lectures", "강의 식별자"));
+
+
     }
 }
